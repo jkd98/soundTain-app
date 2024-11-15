7300f069a0af2941452ed4ad8cac127c8e7ad6e1
jk89 <davidlrj9999@gmail.com>
2024-11-15 09:57:47 -0600

    v2 sin imagenes

--- routes/ordenRoutes.js
+++ routes/ordenRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import checkAuth from "../middleware/chekAuth.js";
-import { ordenCliente } from "../controllers/ordenProductoController.js"; // Importamos el controlador
+import checkRol from "../middleware/checkRol.js";
+import { ordenCliente, ordenesClientes } from "../controllers/ordenProductoController.js"; // Importamos el controlador
 
 const router = express.Router();
 
 // Ruta para listar todos los productos comprados por un cliente
 router.get('/historial-compras', checkAuth, ordenCliente);
+router.post('/historial-ventas', checkAuth, checkRol, ordenesClientes);
 
 export default router;
